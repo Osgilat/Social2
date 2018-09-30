56754ebdde4b36a09515ad853f475f9ed6b7f582
Osgilat <osgilat17@gmail.com>
2018-09-30 13:27:34 +0300

    Attack\Drink functions

--- Social2/Assets/Scripts/AIController.cs
+++ Social2/Assets/Scripts/AIController.cs
@@ -47,6 +47,53 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             
         }
 
+        public void DrinkPotion()
+        {
+            if (GetComponent<PickingUpController>().potion.activeInHierarchy)
+            {
+                anim.Play("Drinking");
+            }
+        }
+
+        public void AttackOnce()
+        {
+            if (GetComponent<PickingUpController>().sword.activeInHierarchy)
+            {
+                int n = UnityEngine.Random.Range(1, 3);
+
+                if (Vector3.Distance(FindClosestEnemy().transform.position, transform.position) < distanceToHit)
+                {
+
+                    FindClosestEnemy().GetComponent<Health>().DecreaseHealth();
+                    Debug.Log("PlayerDamaged");
+                }
+                anim.Play("Slash" + n);
+
+            }
+        }
+
+        public float distanceToHit = 3.0f;
+
+        public GameObject FindClosestEnemy()
+        {
+            GameObject[] gos;
+            gos = GameObject.FindGameObjectsWithTag("Enemy");
+            GameObject closest = null;
+            float distance = Mathf.Infinity;
+            Vector3 position = transform.position;
+            foreach (GameObject go in gos)
+            {
+                Vector3 diff = go.transform.position - position;
+                float curDistance = diff.sqrMagnitude;
+                if (curDistance < distance)
+                {
+                    closest = go;
+                    distance = curDistance;
+                }
+            }
+            return closest;
+        }
+
 
         public void SetTarget(Transform target)
         {
